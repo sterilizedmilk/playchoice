ee6d42600a132d8c9cf1e6b065142f8f1f62f6ed
sterilizedmilk <jungdaia@naver.com>
2018-11-19 20:13:37 +0900

    연극 소개 달력

---
+++ src/main/java/com/playchoice/show/dao/ShowDAOImpl.java
@@ -0,0 +1,8 @@
+package com.playchoice.show.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ShowDAOImpl {
+
+}
---
+++ src/main/java/com/playchoice/show/service/ShowService.java
@@ -0,0 +1,5 @@
+package com.playchoice.show.service;
+
+public interface ShowService {
+
+}
