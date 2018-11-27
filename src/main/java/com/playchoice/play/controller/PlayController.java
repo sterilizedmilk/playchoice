0fa23db19059d5b7d94baf8831bb59cb7dabb407
추윤지 <@401-02-PC>
2018-11-27 10:23:29 +0900

    플레이 리스트

--- src/main/java/com/playchoice/play/controller/PlayController.java
+++ src/main/java/com/playchoice/play/controller/PlayController.java
@@ -39,74 +39,78 @@ public class PlayController {
 		logger.info("list get............");
 		List<PlayDTO> dto = service.playList();
 		model.addAttribute("list", dto);
+		model.addAttribute("title", "전체리스트");
 		return "play/playlist";
 	}
-	
-	//오늘일정
+
+	// 오늘일정
 	@RequestMapping("todaylist")
 	public String playTodaylist(HttpServletRequest request, Model model) throws Exception {
 		HttpSession session = request.getSession();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
-		if(session.getAttribute("date") instanceof Date) {
-			Date date = (Date)session.getAttribute("date");
-			
+		if (session.getAttribute("date") instanceof Date) {
+			Date date = (Date) session.getAttribute("date");
+
 			String res = sdf.format(date);
-			
+
 			List<PlayDTO> dto = service.playTodayList(res);
 			model.addAttribute("list", dto);
+
 		}
+		model.addAttribute("title", "오늘 리스트");
 		return "play/playlist";
 	}
-	
-	//내일일정
+
+	// 내일일정
 	@RequestMapping("tomorrowlist")
 	public String playTomorrowlist(HttpServletRequest request, Model model) throws Exception {
 		HttpSession session = request.getSession();
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		Calendar cal = Calendar.getInstance();
 
-		if(session.getAttribute("date") instanceof Date) {
-			
-			Date date = (Date)session.getAttribute("date");
-			
+		if (session.getAttribute("date") instanceof Date) {
+
+			Date date = (Date) session.getAttribute("date");
+
 			cal.setTime(date);
 			cal.add(Calendar.DATE, 1);
-			
+
 			String res = sdf.format(cal.getTime());
 			List<PlayDTO> dto = service.playTodayList(res);
 			model.addAttribute("list", dto);
 		}
+		model.addAttribute("title", "내일 리스트");
 		return "play/playlist";
 	}
-	
+
 	// 연극 detail
 	@RequestMapping(value = "playdetail", method = RequestMethod.GET)
 	public String playDetail(@RequestParam("p_id") int p_id, Model model) throws Exception {
-		//play
-		model.addAttribute("playDTO",service.playDetail(p_id));
-		
-		//schedule		
-		model.addAttribute("schedule",service.getSchedule(p_id));
+		// play
+		model.addAttribute("playDTO", service.playDetail(p_id));
 
-		//review
-		model.addAttribute("reviewScore",service.getReviewScore(p_id));
+		// schedule
+		model.addAttribute("schedule", service.getSchedule(p_id));
+
+		// review
+		model.addAttribute("reviewScore", service.getReviewScore(p_id));
 		model.addAttribute("reviewSmall", service.getReviewSmall(p_id));
-		
-		//Q & A 
+
+		// Q & A
 		model.addAttribute("qnaAll", service.getQnA(p_id));
 		return "play/playdetail";
-		
+
 	}
-	
-	//장바구니
+
+	// 장바구니
 	@RequestMapping("bastket")
 	public void addBastket(@RequestParam HashMap<String, Object> param) {
 		// TODO 쿠키적용
 		System.out.println(param);
 	}
-	
-	//결제
+
+	// 결제
 	@RequestMapping("payment")
 	public void directPay(@RequestParam HashMap<String, Object> param) {
 		// TODO 로그인여부 확인(인터셉터)
--- src/main/webapp/WEB-INF/views/play/playlist.jsp
+++ src/main/webapp/WEB-INF/views/play/playlist.jsp
@@ -1,5 +1,4 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
@@ -12,24 +11,27 @@
 <jsp:include page="../page/menu.jsp" />
 <meta charset="UTF-8">
 
-<h2 align="center">연극 리스트</h2>
+<h2 align="center">${title}</h2>
 <div class="container" align="center">
 	<div class="row">
-			<c:forEach items="${list}" var="li" varStatus="status">
-				<div style="outline:1px solid #8C8C8C; float:left; margin:15px; width: 180px; height: 320px;">
-					<div>
-						<a href="playdetail?p_id=${li.p_id }">
-						<img src="${pageContext.request.contextPath}/resources/img/play/thumb_${li.p_image0 }"></a>
-					</div>
-					<div>
-						<p> <a href="playdetail?p_id=${li.p_id }">${li.p_name }</a> </p>
-						<p style="float:left; font-style: italic;">
-							최저 가격
-						</p>
-						<p align="right"> <span style="font-size: 20px;  color: #FF5E00">${li.lowest_price}</span> 원 ~ </p>
-					</div>
+		<c:forEach items="${list}" var="li" varStatus="status">
+			<div style="outline: 1px solid #8C8C8C; float: left; margin: 15px; width: 180px; height: 320px;">
+				<div>
+					<a href="playdetail?p_id=${li.p_id }">
+						<img src="${pageContext.request.contextPath}/resources/img/play/thumb_${li.p_image0 }">
+					</a>
 				</div>
-			</c:forEach>
+				<div>
+					<p>
+						<a href="playdetail?p_id=${li.p_id }">${li.p_name }</a>
+					</p>
+					<p style="float: left; font-style: italic;">최저 가격</p>
+					<p align="right">
+						<span style="font-size: 20px; color: #FF5E00">${li.lowest_price}</span> 원 ~
+					</p>
+				</div>
+			</div>
+		</c:forEach>
 	</div>
 </div>
 
